c0776d4c835add56a82e6e7a086c4d6419a48b3d
Vladimir <vladimir_moro@mail.ru>
2022-03-01 20:15:27 +0300

    Гришка эт свежатинка

--- js/js.js
+++ js/js.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-
+/*
     const deadline = '2020-05-11';
     
     function getTimeRemaining(endtime) {
@@ -121,4 +121,49 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+*/
+
+const deadline = '2020-05-11';
+
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }    
+    }
+
+    setClock('.timer', deadline);
+
 });
